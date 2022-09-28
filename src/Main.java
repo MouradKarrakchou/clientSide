d252ec90ba03d99282de7d88ac4cac06b50d2080
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-09-28 15:41:17 +0200

    new Classes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Main {
+    public static void main(String[] args) throws RemoteException, NotBoundException {
+        Registry reg = LocateRegistry.getRegistry("localhost",2001);
+        Distante d = (Distante)reg.lookup("MonOD");
+        Service service= (Service) d.getRMIRerence();
+        service.setValue(5);
+        System.out.println(service.getValue());
+    }
 }
