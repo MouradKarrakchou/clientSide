17a79e1d34bfe0992252bb69534205dcc3801dcd
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-10-04 13:08:00 +0200

    exceptions update

--- src/exception/InvalidCredentialsException.java
+++ src/exception/InvalidCredentialsException.java
@@ -4,6 +4,6 @@ package exception;
  */
 public class InvalidCredentialsException extends Exception {
     public InvalidCredentialsException() {
-        super("Erreur de lors de la connexion, l'email et le mot de passe ne coincident pas");
+        super("Erreur lors de la connexion, l'email et le mot de passe ne coincident pas");
     }
 }
