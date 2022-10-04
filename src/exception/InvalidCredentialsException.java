fe0c65af837579f672f32e0844c0009a94a03ccc
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-10-04 13:06:42 +0200

    exceptions update

--- src/exception/InvalidCredentialsException.java
+++ src/exception/InvalidCredentialsException.java
@@ -1,5 +1,7 @@
 package exception;
-
+/**
+ * Trigger when the client email and password don't mactch
+ */
 public class InvalidCredentialsException extends Exception {
     public InvalidCredentialsException() {
         super("Erreur de lors de la connexion, l'email et le mot de passe ne coincident pas");
--- src/exception/SignInFailed.java
+++ src/exception/SignInFailed.java
@@ -1,5 +1,7 @@
 package exception;
-
+/**
+ * Trigger when the client try to create a new account with an existing email
+ */
 public class SignInFailed extends Exception {
     public SignInFailed() {
         super("Erreur de lors de l'inscription, un compte avec le même email existe déjà");
